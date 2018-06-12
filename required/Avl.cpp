b273fea1ba3542469de55a53d87e715b5e984f67
Alexis Aguirre <alex08lta@gmail.com>
2018-06-12 19:42:05 -0300

    Reordenamiento de funciones en AVL
    Se definen todas las funciones en la parte final
    del archivo.

--- required/Avl.cpp
+++ required/Avl.cpp
@@ -3,79 +3,25 @@ using namespace std;
 class AVL{
 	private:
 		Nodo * raiz;
+		
 	public:
 		//Constructores
 		AVL();
 		AVL(Nodo * n);
 		AVL(string palabra);
+		
 		//metodos generales
-		void toPrint(Nodo *n){
-			//IMPRIME IZQUIERDA, NODO, DERECHA (ORDEN ALFABETICO)
-			if(n != NULL){
-				toPrint(n->getHI());
-				cout << "\n" << n->getPalabra() << endl;
-				toPrint(n->getHD());
-			}
-			return;
-		}
-		int getAltura(){
-			return this->raiz->getAlturaNodo();
-		}
-		
-		bool search(string p, Nodo * origen){
-			string palabraNodo = origen->getPalabra();
-			if(palabraNodo == p){
-				return true;
-			}
-			if(p < palabraNodo && origen->getHI() != NULL)
-				return search(p, origen->getHI());
-			if(p < palabraNodo && origen->getHI() == NULL)
-				return false;
-			if(p > palabraNodo && origen->getHD() != NULL)
-				return search(p, origen->getHD());
-			if(p > palabraNodo && origen->getHD() == NULL)
-				return false;
-			return false;
-		}
-		
-		void balancear(Nodo * nod){
-			if(nod != NULL){
-				//cout << "intentando balancear:" << nod->getPalabra() << endl << endl;
-				this->balancear(nod->getHI());
-				this->balancear(nod->getHD());
-				nod->balancear();
-			}
-			return;
-		}
-		
-		void insertar(string p, Nodo * origen){
-			string palabraNodo = origen->getPalabra();
-			if(palabraNodo == p){
-				origen->aumentarRepeticiones();
-				return;
-			}else	if(p < palabraNodo && origen->getHI() != NULL){
-				return insertar(p, origen->getHI());
-			}else	if(p < palabraNodo && origen->getHI() == NULL){
-				//crear un nuevo nodo con la palabra
-				origen->setHI(new Nodo(p));
-			}else if(p > palabraNodo && origen->getHD() != NULL){
-				return insertar(p, origen->getHD());
-			}else if(p > palabraNodo && origen->getHD() == NULL){
-				//crear un nuevo nodo con la palabra
-				origen->setHD(new Nodo(p));
-			}
-			this->balancear(this->raiz);
-			return;
-		}
+		void toPrint(Nodo *n);
+		bool search(string p, Nodo * origen);
+		void balancear(Nodo * nod);
+		void insertar(string p, Nodo * origen);
 		
 		//getters
-		Nodo * getRaiz(){
-			return this->raiz;
-		};
+		Nodo * getRaiz();
+		int getAltura();
+		
 		//setters
-		void setRaiz(Nodo * n){
-			this->raiz = n;
-		};
+		void setRaiz(Nodo * n);
 };
 
 AVL::AVL(){
@@ -89,4 +35,71 @@ AVL::AVL(string cadena){
 	this->raiz = new Nodo(cadena);
 }
 
+void AVL::toPrint(Nodo * n){
+	//IMPRIME IZQUIERDA, NODO, DERECHA (ORDEN ALFABETICO)
+	if(n != NULL){
+		toPrint(n->getHI());
+		cout << "\n" << n->getPalabra() << endl;
+		toPrint(n->getHD());
+	}
+	return;
+}
 
+bool AVL::search(string p, Nodo * origen){
+	string palabraNodo = origen->getPalabra();
+	if(palabraNodo == p){
+		return true;
+	}
+	if(p < palabraNodo && origen->getHI() != NULL)
+		return search(p, origen->getHI());
+	if(p < palabraNodo && origen->getHI() == NULL)
+		return false;
+	if(p > palabraNodo && origen->getHD() != NULL)
+		return search(p, origen->getHD());
+	if(p > palabraNodo && origen->getHD() == NULL)
+		return false;
+	return false;
+}
+
+void AVL::balancear(Nodo * nod){
+	if(nod != NULL){
+		this->balancear(nod->getHI());
+		this->balancear(nod->getHD());
+		nod->balancear();
+	}
+	return;
+}
+
+void AVL::insertar(string p, Nodo * origen){
+	string palabraNodo = origen->getPalabra();
+	if(palabraNodo == p){
+		origen->aumentarRepeticiones();
+		return;
+	}else	if(p < palabraNodo && origen->getHI() != NULL){
+		return insertar(p, origen->getHI());
+	}else	if(p < palabraNodo && origen->getHI() == NULL){
+		//crear un nuevo nodo con la palabra
+		origen->setHI(new Nodo(p));
+	}else if(p > palabraNodo && origen->getHD() != NULL){
+		return insertar(p, origen->getHD());
+	}else if(p > palabraNodo && origen->getHD() == NULL){
+		//crear un nuevo nodo con la palabra
+		origen->setHD(new Nodo(p));
+	}
+	this->balancear(this->raiz);
+	return;
+}
+
+//GETTERS
+Nodo * AVL::getRaiz(){
+	return this->raiz;
+}
+
+int AVL::getAltura(){
+	return this->raiz->getAlturaNodo();
+}
+
+//SETTERS
+void AVL::setRaiz(Nodo * n){
+	this->raiz = n;
+}
